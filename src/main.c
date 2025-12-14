6371f24eda564a70014a8116be5cd34735247aaf
kwaters <waters.kevin.r@gmail.com>
2025-12-13 21:34:37 -0500

    Format print, add python tester

--- src/main.c
+++ src/main.c
@@ -4,6 +4,20 @@
 #include <matrix_mul.h>
 #include <timer.h>
 
+void print_header(){
+printf("n, trials, req. memory (KiB), time/trial (s), IPC, Read Bandwidth (GiB/s)\n");
+
+}
+
+void print_stats(struct timer *t, int n, int trials){
+  // Matrix memory in KiB
+  int req_memory = 3*n*n*sizeof(int)/1024;
+  double ipc = timer_calc_ipc(t);
+  // Read Bandwitdh in GiB/s
+  double bandwidth = (double) ((2*n*n * sizeof(int)))/(1024*1024*1024) / t->elapsed;
+  printf("%d, %d, %d, %f, %f, %f \n", n, trials, req_memory, t->elapsed/trials, ipc, bandwidth);
+}
+
 int main(int argc, char **argv) {
 
   if (argc < 3) {
@@ -13,11 +27,12 @@ int main(int argc, char **argv) {
 
   size_t min_size = atol(argv[1]);
   size_t max_size = atol(argv[2]);
-  size_t num_trials = atol(argv[3]);
+  int num_trials = atol(argv[3]);
 
   //Set random seed for matrix generation.
   srand(0);
   struct timer matrix_timer;
+  print_header();
   for(size_t i = min_size; i<= max_size; i=2*i) {
     int req_memory = 3*i*i*sizeof(int);
     int *matrix1 = malloc((i*i) * sizeof(int));
@@ -37,19 +52,16 @@ int main(int argc, char **argv) {
     populate_matrix(matrix1, i);
     populate_matrix(matrix2, i);
     generate_zero_matrix(result, i);
+    // Warm up run
+    multiply_matrices(matrix1, matrix2, result, i);
     timer_start(&matrix_timer, num_trials * matrix_work);
-    printf("Multiplying %ldx%ld matrices... (Trials:%ld)\n",i ,i, num_trials);
     for (int trial = 0; trial < num_trials; trial ++){
 //      multiply_matrices(matrix1, matrix2, result, i);
-//      multiply_matrices_sum(matrix1, matrix2, result, i);
-      multiply_matrices_strip(matrix1, matrix2, result, i);
+      multiply_matrices_sum(matrix1, matrix2, result, i);
+//      multiply_matrices_strip(matrix1, matrix2, result, i);
     }
     timer_stop(&matrix_timer);
-    double ipc = timer_calc_ipc(&matrix_timer);
-    printf("Mem Req. (KiB) %d \n", req_memory/1024);
-    printf("Elasped Time (s.): %f\n", matrix_timer.elapsed);
-    printf("Calculated IPC: %f\n", ipc);
-    printf("Read Memory Bandwidth (GiB/s): %f\n",(double) ((2*i*i * sizeof(int)))/(1024*1024*1024) / matrix_timer.elapsed);
+    print_stats(&matrix_timer, i, num_trials);
 
     free(matrix1);
     free(matrix2);
